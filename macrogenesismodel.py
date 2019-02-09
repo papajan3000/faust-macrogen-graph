8e6c761503ef324f6812d98374c3a856241706cc
papajan3000 <janpaulus95@web.de>
2019-02-09 17:44:57 +0100

    feat: implementation of eades algorithm, yet still in progress

--- macrogenesismodel.py
+++ macrogenesismodel.py
@@ -8,24 +8,20 @@ filespath = Path('resources')
 items = parserutils.xmlparser(filespath)
 print(items[2])
 #%%
-
+#####
+# graph for temppre <relation>-elements
+#####
 
 #TODO: remove the enumerate
 G = nx.DiGraph()
 for idx, t in enumerate(items):
-    if idx < 15:
+    if idx < 100000:
         graphutils.add_egdes_from_node_list(G, t)
     else:
         break
   
 nx.draw_networkx(G)
 plt.show()
-
-print(nx.is_directed_acyclic_graph(G))
-
-
-
-#%%
-
 nx.write_graphml(G, "graphs/g4.graphml")
 
+#%%
