c85038b3262fdb9f9044a3855e977440fdc2cf92
papajan3000 <janpaulus95@web.de>
2019-02-12 14:48:27 +0100

    feat: FAS of faust-graph

--- macrogenesismodel.py
+++ macrogenesismodel.py
@@ -1,27 +1,57 @@
 # -*- coding: utf-8 -*-
-from faust_macrogen_graph import parserutils, graphutils
+from faust_macrogen_graph import parserutils, graphutils, eades_fas
 from pathlib import Path
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
 filespath = Path('resources')
 items = parserutils.xmlparser(filespath)
-print(items[2])
 #%%
 #####
 # graph for temppre <relation>-elements
 #####
 
-#TODO: remove the enumerate
+
 G = nx.DiGraph()
-for idx, t in enumerate(items):
-    if idx < 100000:
-        graphutils.add_egdes_from_node_list(G, t)
-    else:
-        break
-  
+for t in items:
+    graphutils.add_egdes_from_node_list(G, t)
+
 nx.draw_networkx(G)
 plt.show()
-nx.write_graphml(G, "graphs/g4.graphml")
+#nx.write_graphml(G, "graphs/g5.graphml")
 
 #%%
+# seven percent of the edges are in the FAS
+
+fas = eades_fas.eades_FAS(G, True)
+print(type(fas))
+
+####
+# HIER WEITER
+# keine doppeltn (also keine (v,u) bei (u,v))
+#
+# Aufgabe a, b, c machen?!
+#
+#
+
+#%%
+
+G2 = G.copy()
+
+G2.remove_edges_from(fas)
+
+c = 0
+for component in nx.connected_component_subgraphs(G.to_undirected()):
+    c += len(component.edges())
+    print(len(component.edges()))
+print("-")
+print(c)
+#%%
+print(len(G.edges()))
+print(len(G2.edges()))
+
+print(nx.is_directed_acyclic_graph(G2))
+
+
+
