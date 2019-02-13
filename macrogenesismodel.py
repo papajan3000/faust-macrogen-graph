edac0a95b2a7d8d924b92c57195c353906edada0
papajan3000 <janpaulus95@web.de>
2019-02-13 15:16:07 +0100

    refactor: renaming graphs and adding temp-syn graph + fas

--- macrogenesismodel.py
+++ macrogenesismodel.py
@@ -6,43 +6,103 @@ import matplotlib.pyplot as plt
 
 
 filespath = Path('resources')
-items = parserutils.xmlparser(filespath)
+temppre_items = parserutils.xmlparser(filespath)
+tempsyn_items = parserutils.xmlparser(filespath, False, False)
+#date_items = ...
+
+#%%
+#####
+# graph for tempsyn <relation>-elements
+#####
+
+#tpG = temppre Graph
+tempsynG = nx.DiGraph()
+for t in tempsyn_items:
+    graphutils.add_egdes_from_node_list(tempsynG, t)
+
+#print(list(tempsynG.nodes()))
+    
+    
+pos = nx.shell_layout(tempsynG)
+nx.draw_networkx_nodes(tempsynG, pos)
+nx.draw_networkx_labels(tempsynG, pos)
+nx.draw_networkx_edges(tempsynG, pos)    
+    
+#nx.draw_networkx(tempsynG)
+plt.show()
+print(nx.is_directed_acyclic_graph(tempsynG))
+
+#%%
+tempsynG_fas = eades_fas.eades_FAS(tempsynG, True) # seven percent of the edges of tpG are in the FAS
+print(tempsynG_fas)
+
+#atempysnG = acyclic tempsyn Graph
+atempsynG = tempsynG.copy()
+atempsynG.remove_edges_from(tempsynG_fas)
+nx.draw_networkx(atempsynG)
+plt.show()
+print(nx.is_directed_acyclic_graph(atempsynG))
+
+
+
 #%%
 #####
 # graph for temppre <relation>-elements
 #####
 
+#tpG = temppre Graph
+temppreG = nx.DiGraph()
+for t in temppre_items:
+    graphutils.add_egdes_from_node_list(temppreG, t)
 
-G = nx.DiGraph()
-for t in items:
-    graphutils.add_egdes_from_node_list(G, t)
 
-nx.draw_networkx(G)
+nx.draw_networkx(temppreG)
 plt.show()
-#nx.write_graphml(G, "graphs/g5.graphml")
+
+
 
 #%%
-# seven percent of the edges are in the FAS
+l = {}
+for e in temppreG.edges():
+    l[e] =temppreG.get_edge_data(e[0], e[1])
+print(len(l))
+#%%
+temppreG_fas = eades_fas.eades_FAS(temppreG, True) # seven percent of the edges of tpG are in the FAS
 
-fas = eades_fas.eades_FAS(G, True)
-print(type(fas))
+#atemppreG = acyclic temppre Graph
+atemppreG = temppreG.copy()
+atemppreG.remove_edges_from(temppreG_fas)
+
+
 
 ####
 # HIER WEITER
-# keine doppeltn (also keine (v,u) bei (u,v))
+# keine doppelten (also keine (v,u) bei (u,v))
 #
 # Aufgabe a, b, c machen?!
 #
 #
+#%%
+
+fas_relation_overlap = []
+
+for edges in tempsynG_fas:
+    if edges in temppreG_fas:
+        fas_relation_overlap.append(edges)
+
+#should be empty       
+print(fas_relation_overlap)
+
+
 
 #%%
 
-G2 = G.copy()
+G2 = tpG.copy()
 
 G2.remove_edges_from(fas)
 
 c = 0
-for component in nx.connected_component_subgraphs(G.to_undirected()):
+for component in nx.connected_component_subgraphs(tpG.to_undirected()):
     c += len(component.edges())
     print(len(component.edges()))
 print("-")
