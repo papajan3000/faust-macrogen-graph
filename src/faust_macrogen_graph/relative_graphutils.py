f16cd1478b2130e92c8e9804e6bf8f27b0359f50
papajan3000 <janpaulus95@web.de>
2019-02-23 15:33:28 +0100

    fix: other variable names for function

--- src/faust_macrogen_graph/relative_graphutils.py
+++ src/faust_macrogen_graph/relative_graphutils.py
@@ -1,23 +1,16 @@
 #%%
-#TODO: docstrings
-import networkx as nx
-from collections import Counter
-import re
-from datetime import datetime
-
-
-def add_egdes_from_node_list(G, node_list):
+def add_egdes_from_node_list(G, nodelist):
     """Add an edge from every node of a list of nodes to the next node in the list with the source as edge attribute.
     
     Args:
         G (DiGraph): DiGraph-Object of networkx.
-        node_list (list): list with 2-tuples, where the first item is a list of sources and the second item a tuple of nodes in a given order.
+        nodelist (list): list with 2-tuples, where the first item is a list of sources and the second item is a tuple of nodes in a given order.
     Returns:
-        Enhanced input-graph with the nodes and edges of the node_list and the source as edge attribute.
+        Enhanced input-graph with the nodes and edges of the nodelist and the source as edge attribute.
     """
-    #only first source is taken if more than one source exist
-    source_name = node_list[0][0]
-    node_tuple = node_list[1]
+    #only first source is taken if more than one source exists
+    source_name = nodelist[0][0]
+    node_tuple = nodelist[1]
     nG = G
     for index, node in enumerate(node_tuple):
         current_node = node
@@ -31,4 +24,3 @@ def add_egdes_from_node_list(G, node_list):
         else:
             nG.add_edge(current_node, next_node, weight=1.0, source=source_name)
     return nG
-
