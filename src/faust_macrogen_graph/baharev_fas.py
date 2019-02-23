11e572eacd53b90467a9d31633b632577220ba9a
papajan3000 <janpaulus95@web.de>
2019-02-23 15:32:49 +0100

    add: some ideas for baharev_fas

--- src/faust_macrogen_graph/baharev_fas.py
+++ src/faust_macrogen_graph/baharev_fas.py
@@ -1,12 +1,68 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from collections import deque
+import itertools
+
+
+
+def to_cycle(simple_path_nodes):
+    seq = rotate_min_to_first_pos(simple_path_nodes)
+    a, b = itertools.tee(seq)
+    next(b, None)
+    return tuple( izip_longest(a, b, fillvalue=seq[0]) )
+
+def get_all_cycles(G, cutoff=500):
+    # Try to enumerate cutoff+1 simple cycles
+    cycles = list(itertools.islice(nx.simple_cycles(G), cutoff+1))
+    # If that succeeds, we give up
+    if len(cycles) > cutoff:
+        print('More than', cutoff, 'simple cycles, giving up...')
+        return False, None
+    # Otherwise we have enumerated ALL cycles, we return the edges of each
+    edges_per_cycle = [to_cycle(c) for c in cycles ]
+    return True, edges_per_cycle
+
+
+
+#TODO: get all cycles von baharev kopieren und einfacher anwenden auf baharev_fas
+def edges_per_cycle(G):
+    ...
+
+#TODO: is 1 if edge j participates in cycle i and 0 otherwise
+def calc_y(j, fas):
+    """
+    
+    Returns:
+        1 if edge j participates in cycle i and 0 otherwise.
+    """
+    
+    ...
+    
+#TODO: calculate the matrix A called the cycle matrix
+def calc_cyclematrix(G):
+    ...
+#%%
+#####
+# main baharev fas
+#####
 
 def baharev_FAS(G):
     """
         Args:
             G (DiGraph): A Directed Graph (networkx DiGraph) with cycles and nonnegative edge weights.
     """
+    m = list(G.edges())
+    #w = weigths of the edges
+    l = len(list(nx.simple_cycles(G)))
+    
+    for j in m:
+        u = j[0]
+        v = j[1]
+        w = G[u][v]["weight"]
+        
+        y = calc_y(j, fas)
+    
+    
     fas = set()
     
     
@@ -60,7 +116,15 @@ if __name__ == '__main__':
         print("The graph contains cycles.")
     else:
        print("The graph contains no cycles.")
-       
+    
+    
+#%%
+    print(list(G.edges()))
+    print(G["A"]["B"]["weight"])
+    
+    
+    
+#%%
     """
     fas = eades_FAS(G, True)
     print("\nThese are the edges from the Feedback Arc Set, which will be removed from the graph: " + str(fas) + "\n")
